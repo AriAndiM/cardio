3d23128b78938b12b63d34d424d9c4a471544d0d
Ari Andi Mustofa <83968034+AriAndiM@users.noreply.github.com>
2022-11-30 15:03:18 +0700

    update akurasi model

--- home.py
+++ home.py
@@ -176,7 +176,7 @@ with st.container():
         gnb = GaussianNB()
         gnb.fit(training, training_label)
         acc_gnb = gnb.score(test, test_label)
-        st.subheader('Akurasi Gaussian : ', acc_gnb*100, '%')
+        st.write('Akurasi Gaussian : ', acc_gnb*100, '%')
         
         st.subheader("2. KNN")
         st.caption("Algoritma K-Nearest Neighbor (KNN) adalah sebuah metode klasifikasi terhadap sekumpulan data berdasarkan pembelajaran data yang sudah terklasifikasikan sebelumya. Termasuk dalam supervised learning, dimana hasil query instance yang baru diklasifikasikan berdasarkan mayoritas kedekatan jarak dari kategori yang ada dalam K-NN. Tujuan dari algoritma ini adalah untuk mengklasifikasikan obyek baru berdasarkan atribut dan sample-sample dari training data.")
@@ -186,7 +186,7 @@ with st.container():
         knn = KNeighborsClassifier(n_neighbors=5)
         knn.fit(training, training_label)
         acc_knn = knn.score(test, test_label)
-        st.subheader('Akurasi KNN : ', acc_knn*100, '%')
+        st.write('Akurasi KNN : ', acc_knn*100, '%')
 
         st.subheader("3. Decision Tree")
         st.caption("Decision tree adalah algoritma machine learning yang menggunakan seperangkat aturan untuk membuat keputusan dengan struktur seperti pohon yang memodelkan kemungkinan hasil, biaya sumber daya, utilitas dan kemungkinan konsekuensi atau resiko. Konsepnya adalah dengan cara menyajikan algoritma dengan pernyataan bersyarat, yang meliputi cabang untuk mewakili langkah-langkah pengambilan keputusan yang dapat mengarah pada hasil yang menguntungkan. ")
@@ -198,7 +198,7 @@ with st.container():
         dt = DecisionTreeClassifier()
         dt.fit(training, training_label)
         acc_dt = dt.score(test, test_label)
-        st.subheader('Akurasi Decision Tree : ', acc_dt*100, '%')
+        st.write('Akurasi Decision Tree : ', acc_dt*100, '%')
 
     elif choose == "Predict":
 
