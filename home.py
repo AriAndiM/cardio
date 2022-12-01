4e19ce5f7b507de1c7f7e286bd0174ed30f25249
AriAndiM <andyary961@gmail.com>
2022-12-01 23:54:46 +0700

    update bg hover sidebar

--- home.py
+++ home.py
@@ -17,7 +17,7 @@ with st.container():
                              styles={
             "container": {"padding": "5!important", "background-color": "#fafafa"},
             "icon": {"color": "orange", "font-size": "25px"}, 
-            "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+            "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "10A19D"},
             "nav-link-selected": {"background-color": "#85BAFF"},
         }
         )
