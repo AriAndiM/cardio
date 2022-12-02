069a5dd1d7b8a0a0e8d5b7062733fc8e422af072
AriAndiM <andyary961@gmail.com>
2022-12-03 01:57:33 +0700

    update form predict

--- home.py
+++ home.py
@@ -227,9 +227,9 @@ with st.container():
         # # inputan_num
         # inputan_biner = [gender,kolestrol, glukosa, merokok, alkohol, aktivitas]
         # inputan_biner
-        data_norm_min = data_norm.min()
-        data_norm_max = data_norm.max()
-        norm_input = ((inputan_num - data_norm_min)/(data_norm_max - data_norm_min))
+        x_min = x.min()
+        x_max = x.max()
+        norm_input = ((inputan_num - x_min)/(x_max - x_min))
         
         # join = np.concatenate((norm_input, inputan_biner))
         # join
